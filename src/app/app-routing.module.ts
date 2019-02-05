7f4b26ba5778ddfdc9cd6d522a49f8b83ff9b797
Space Cow <mattcampen@gmail.com>
2019-02-05 13:52:32 -0600

    Added user edit page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { UsersComponent }   from './users/users.component';
+import { UsersComponent } from './users/users.component';
 import { UserViewComponent } from './user-view/user-view.component';
-import { UserCreateComponent }   from './user-create/user-create.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
-  {path: 'users/view/:id', component: UserViewComponent},
-  { path: 'users/create', component: UserCreateComponent }
+  { path: 'users/view/:id', component: UserViewComponent },
+  { path: 'users/create', component: UserCreateComponent },
+  { path: 'users/edit/:id', component: UserEditComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { UsersService} from './users.service';
 import { UserViewComponent } from './user-view/user-view.component';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     UserViewComponent,
-    UserCreateComponent
+    UserCreateComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
