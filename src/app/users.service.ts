f541c3ffd8385bf668411d3ebb2fa79bd16ba8e9
Space Cow <mattcampen@gmail.com>
2019-02-05 15:06:54 -0600

    added edit and delete functionality to user view

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -25,8 +25,16 @@ export class UsersService {
     return this.http.get<User>(`${this.url}/${id}`);
   }
 
-  createUser (user: User): Observable<User> {
+  createUser(user: User): Observable<User> {
     return this.http.post<User>(this.url, user, httpOptions);
+  }
+
+  editUser(user: User): Observable<User> {
+    return this.http.put<User>(this.url, user, httpOptions);
+  }
+
+  deleteUser(id: string): Observable<User> {
+    return this.http.delete<User>(`${this.url}/${id}`);
   }
 
 }
