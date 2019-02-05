46aeca52d11b1637e9b6bc9f910cb79b9c888b5f
Space Cow <mattcampen@gmail.com>
2019-02-05 13:44:18 -0600

    Added user create details

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -25,4 +25,8 @@ export class UsersService {
     return this.http.get<User>(`${this.url}/${id}`);
   }
 
+  createUser (user: User): Observable<User> {
+    return this.http.post<User>(this.url, user, httpOptions);
+  }
+
 }
